e99e848cf53e1c66e936ce92e3c42dd5e15e1825
Adan Carrasco <adan.carrasco@hotmail.com>
2019-02-10 17:51:13 -0600

    Add common params to OpenWeatherApi

--- src/utils/OpenWeatherApi.js
+++ src/utils/OpenWeatherApi.js
@@ -1,14 +1,19 @@
 import axios from "axios";
+import { OPEN_WEATHER_APP_ID } from "../utils/env";
 
 const API_BASE_URL = "https://api.openweathermap.org/data/";
 const API_VERSION = "2.5";
+const UNITS = "metric";
 
 class OpenWeatherApi {
   constructor() {
     this._axios = axios.create({
       baseURL: this.getBaseUrl(),
-
-    })
+      params: {
+        APPID: OPEN_WEATHER_APP_ID,
+        units: UNITS
+      }
+    });
   }
 
   getBaseUrl() {
@@ -19,7 +24,7 @@ class OpenWeatherApi {
     return this._axios.get(url, config);
   }
 
-  // All other verbs can be defined here 
+  // All other verbs can be defined here
 }
 
 export default new OpenWeatherApi();
