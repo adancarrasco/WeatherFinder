e97d8ef1deac21cddba7148a3a82f34d8a328fd8
Adan Carrasco <adan.carrasco@hotmail.com>
2019-02-10 19:53:41 -0600

    Show cards for Berlin and Waltham cities in Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import MainContainer from "../components/MainContainer";
 import WeatherCard from "../components/WeatherCard";
+import defaultCities from "../utils/defaultCities";
 
 const Home = () => {
   return (
     <MainContainer>
-      <WeatherCard cityName="Berlin" />
+      {defaultCities.map(cityName => <WeatherCard key={cityName} cityName={cityName} />)}
     </MainContainer>
   );
 };
