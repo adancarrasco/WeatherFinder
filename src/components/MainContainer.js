319a5e8b7ec16bbb6ef6bf18de3e644a7e106e85
Adan Carrasco <adan.carrasco@hotmail.com>
2019-02-10 18:25:43 -0600

    Update MainContainer to append children

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import styles from "./MainContainer.module.css";
 
-const MainContainer = () => {
-  return <div className={styles.mainContainer} />;
+const MainContainer = props => {
+  return <div className={styles.mainContainer}>{props.children}</div>;
 };
 
 export default MainContainer;
--- src/components/tests/MainContainer.spec.js
+++ src/components/tests/MainContainer.spec.js
@@ -4,8 +4,12 @@ import { shallow } from "enzyme";
 import MainContainer from "../MainContainer";
 
 describe("MainContainer", () => {
-  it("it should render correctly", () => {
-    const wrapper = shallow(<MainContainer />);
+  it("it should render correctly with children", () => {
+    const wrapper = shallow(
+      <MainContainer>
+        <div />
+      </MainContainer>
+    );
     expect(wrapper.debug()).toMatchSnapshot();
   });
 });
