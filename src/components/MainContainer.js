bb5686d855ac21794d4069e9af1aec181225cdec
Adan Carrasco <adan.carrasco@hotmail.com>
2019-02-10 18:22:05 -0600

    Create MainContainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./MainContainer.module.css";
+
+const MainContainer = () => {
+  return <div className={styles.mainContainer} />;
+};
+
+export default MainContainer;
