82397902fff0e9ada1cdbeec833ab84abe4af057
Adan Carrasco <adan.carrasco@hotmail.com>
2019-02-10 18:31:07 -0600

    Add MainContainer to App.js and remove dead code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
+import React, { Component } from "react";
+
+import MainContainer from "./components/MainContainer";
 
 class App extends Component {
   render() {
-    return (
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <MainContainer />;
   }
 }
 
